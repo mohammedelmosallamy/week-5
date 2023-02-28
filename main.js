c9330ff1cc79083e6767cc22765a4d351f7e3b2f
mohammedelmosallamy <mohammedelmosallamy96@gmail.com>
2023-02-28 18:19:07 +0200

    week 5 task

---
+++ main.js
@@ -0,0 +1,23 @@
+// ==========Write a JS code to print numbers from 1 to 10-------------------->
+
+for(var x = 0; x <=10; x++){
+    console.log(x)
+}
+
+// ==========Write a JavaScript 3methods  to print the “Hello World” message-------------------->
+
+var y = "hello world"
+console.log(y.toUpperCase(y))
+console.log(y.split("").reverse().join(""))
+console.log(y.replace("h","x"))
+
+// ==========Write a JS code to print Even numbers in given array-------------------->
+
+var num= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+var evennum= num.filter((num) => num% 2 === 0);
+console.log(evennum)
+
+// ==========Write a JS code to print a pattern using for loop-------------------->
+
+var z =0; z<=20,z++;
+console.log(z)
---
+++ number square/methodd 2/square number.js
@@ -0,0 +1,4 @@
+// ==========Write a function that returns the square of a number-------------------->
+
+var num = prompt(`please enter the number:`)
+document.write(Math.pow(num,2))
